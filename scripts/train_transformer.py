e3cacc1a7995e4e63b5f9395f2c95083ac061e02
Rwigle <109574721+Rwigie@users.noreply.github.com>
2025-12-04 14:43:59 -0800

    Add files via upload

---
+++ scripts/train_transformer.py
@@ -0,0 +1,40 @@
+
+modelName = 'Log-PatchMask-Transformer'
+
+args = {}
+args['model'] = 'Transformer'
+args['outputDir'] = 'logs/speech_logs/' + modelName
+args['datasetPath'] = 'datasets/ptDecoder_ctc'
+args['load_weights'] = False
+args['load_weights_path'] = 'logs/speech_logs/speechBaseline-PatchMask-Transformer/modelWeights'
+args['seqLen'] = 150
+args['maxTimeSeriesLen'] = 1200
+args['batchSize'] = 64
+args['lrStart'] = 1e-3
+args['lrEnd'] = 1e-5
+args['nUnits'] = 256
+args['nhead'] = 8
+args['nBatch'] = 13000 #3000
+args['nLayers'] = 5
+args['seed'] = 0
+args['nClasses'] = 40
+args['nInputFeatures'] = 256
+args['dropout'] = 0.4
+args['whiteNoiseSD'] = 1.0
+args['constantOffsetSD'] = 0.2
+args['gaussianSmoothWidth'] = 2.0
+args['strideLen'] = 5
+args['kernelLen'] = 5
+args['bidirectional'] = True
+args['l2_decay'] = 1e-5
+args['temporalMaxLen'] = 0
+args['temporalMaskProb'] = 1
+args['temporalNumMask'] = 4
+args['patchMaskRatio'] = 0.075
+args['patchNumMask'] = 20
+args['causal'] = False
+
+from src.neural_decoder.neural_decoder_trainer import trainModel
+print(args)
+trainModel(args)
+#export PYTHONPATH=$PWD:$PYTHONPATH
